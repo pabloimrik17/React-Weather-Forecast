bb24887bd78e592d1e0928d98a58ad502bd415ed
Etherless <pabloimrik17@gmail.com>
2017-11-26 19:19:23 +0100

    WeatherList component into functional component approach

--- src/components/Weather/WeatherList.js
+++ src/components/Weather/WeatherList.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import WeatherListItem from "./WeatherListItem";
 
-class WeatherList extends React.Component {
-    render() {
-        const {days, onDayClicked} = this.props;
-
-        return (
-            <div className="weather-list flex-parent">
-                {days.map((day, index) =>
-                    <WeatherListItem
-                        key={day.dt}
-                        day={day}
-                        onDayClicked={onDayClicked}
-                        index={index}
-                    />
-                )};
-            </div>
-        );
-    }
-}
+// Functional Component way (only render function) + Ecma 6
+const WeatherList = ({days, onDayClicked}) =>
+    <div className="weather-list flex-parent">
+        {days.map((day, index) =>
+            <WeatherListItem
+                key={day.dt}
+                day={day}
+                onDayClicked={onDayClicked}
+                index={index}
+            />
+        )};
+    </div>;
 
 export default WeatherList;
